d85b748bd41fd362a8d8af4e5daabf19dc95d5ef
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-07-17 20:47:33 -0500

    Building of the UI which includes the video called from the flutter campus web page, the total duration of the video, a video progress indicator and two buttons (a play pause one and a stop one). The logic behind the buttons has been performed using the methods offered by video player plugin. Ill take this as my main guide to implement a similar functionality in my institutional project.

--- lib/main.dart
+++ lib/main.dart
@@ -53,9 +53,55 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Video Player in Flutter'),
+        backgroundColor: Colors.redAccent,
       ),
-      body: Center(),
+      body: Container(
+          child: Column(children: [
+        AspectRatio(
+          aspectRatio: controller.value.aspectRatio,
+          child: VideoPlayer(controller),
+        ),
+        Container(
+          //duration of video
+          child:
+              Text("Total Duration: " + controller.value.duration.toString()),
+        ),
+        Container(
+            child: VideoProgressIndicator(controller,
+                allowScrubbing: true,
+                colors: VideoProgressColors(
+                  backgroundColor: Colors.redAccent,
+                  playedColor: Colors.green,
+                  bufferedColor: Colors.purple,
+                ))),
+        Container(
+          child: Row(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    if (controller.value.isPlaying) {
+                      controller.pause();
+                    } else {
+                      controller.play();
+                    }
+
+                    setState(() {});
+                  },
+                  icon: Icon(controller.value.isPlaying
+                      ? Icons.pause
+                      : Icons.play_arrow)),
+              IconButton(
+                  onPressed: () {
+                    controller.seekTo(Duration(seconds: 0));
+
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.stop))
+            ],
+          ),
+        )
+      ])),
     );
   }
 }
