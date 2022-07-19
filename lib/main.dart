3ac3415ee91fb4673c45df5cd59d6140da449643
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-07-18 23:17:27 -0500

    Improving the appearance of the UI by selecting some colors (adding some style), distributing spaces between elements and arranging dimensions.

--- lib/main.dart
+++ lib/main.dart
@@ -68,52 +68,66 @@ class _MyHomePageState extends State<MyHomePage> {
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             return Container(
+                color: Colors.red[100],
                 child: Column(children: [
-              SizedBox(height: 25),
-              AspectRatio(
-                aspectRatio: controller.value.aspectRatio,
-                child: VideoPlayer(controller),
-              ),
-              Container(
-                //duration of video
-                child: Text(
-                    "Total Duration: " + controller.value.duration.toString()),
-              ),
-              Container(
-                  child: VideoProgressIndicator(controller,
-                      allowScrubbing: true,
-                      colors: VideoProgressColors(
-                        backgroundColor: Colors.redAccent,
-                        playedColor: Colors.green,
-                        bufferedColor: Colors.purple,
-                      ))),
-              Container(
-                child: Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {
-                          if (controller.value.isPlaying) {
-                            controller.pause();
-                          } else {
-                            controller.play();
-                          }
+                  SizedBox(height: 35),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: AspectRatio(
+                      aspectRatio: controller.value.aspectRatio,
+                      child: VideoPlayer(controller),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Container(
+                    //duration of video
+                    child: Text("Total Duration: " +
+                        controller.value.duration.toString()),
+                  ),
+                  SizedBox(height: 5),
+                  Container(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: VideoProgressIndicator(controller,
+                          allowScrubbing: true,
+                          colors: VideoProgressColors(
+                            backgroundColor: Colors.redAccent,
+                            playedColor: Colors.green,
+                            bufferedColor: Colors.purple,
+                          ))),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              if (controller.value.isPlaying) {
+                                controller.pause();
+                              } else {
+                                controller.play();
+                              }
 
-                          setState(() {});
-                        },
-                        icon: Icon(controller.value.isPlaying
-                            ? Icons.pause
-                            : Icons.play_arrow)),
-                    IconButton(
-                        onPressed: () {
-                          controller.seekTo(Duration(seconds: 0));
+                              setState(() {});
+                            },
+                            iconSize: 36,
+                            color: Colors.blueAccent,
+                            icon: Icon(controller.value.isPlaying
+                                ? Icons.pause
+                                : Icons.play_arrow)),
+                        IconButton(
+                          onPressed: () {
+                            controller.seekTo(Duration(seconds: 0));
 
-                          setState(() {});
-                        },
-                        icon: Icon(Icons.stop))
-                  ],
-                ),
-              )
-            ]));
+                            setState(() {});
+                          },
+                          icon: Icon(Icons.stop),
+                          iconSize: 36,
+                          color: Colors.blueAccent,
+                        ),
+                      ],
+                    ),
+                  )
+                ]));
           } else {
             return Center(
               child: SizedBox(
